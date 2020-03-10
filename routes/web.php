73a6a0eb6487d98e5994a1e15f5a58002c8dd950
Prashanthi Ramesh <prashanthi.ramesh@mcit.local>
2020-03-09 20:23:23 -0400

    - Fix for route for home

--- routes/web.php
+++ routes/web.php
@@ -29,9 +29,6 @@ Auth::routes();
 
 Route::group(['middleware' => ['auth']], function () {
 
-    Route::get('/', function(){
-        return view('home');
-    });
-    
+    Route::get('/', 'HomeController@index')->name('home');
     Route::get('/home', 'HomeController@index')->name('home');
 });
